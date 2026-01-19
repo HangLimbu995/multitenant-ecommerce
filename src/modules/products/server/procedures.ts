783f0b33900ef9d863d08691594523efe7f91808
Khesehang's Mac <khesehangsmac@Khesehangss-MacBook-Pro.local>
2026-01-19 13:48:04 +0545

    13: api filters and sorting

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -2,8 +2,9 @@
 import z from 'zod'
 
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-import { Where } from 'payload';
+import { Sort, Where } from 'payload';
 import { Category } from '@/payload-types';
+import { sortValues } from '../search-params';
 
 
 type CategoryWithSubcategories = Category & {
@@ -19,11 +20,28 @@ export const productsRouter = createTRPCRouter({
         category: z.string().nullable().optional(),
         minPrice: z.string().nullable().optional(),
         maxPrice: z.string().nullable().optional(),
+        tags: z.array(z.string()).nullable().optional(),
+        sort: z.enum(sortValues).nullable().optional()
       })
     )
     .query(async ({ ctx, input }) => {
       const where: Where = {
       }
+
+      let sort: Sort = "-createdAt";
+
+      if(input.sort === 'curated') {
+        sort = '-createdAt'
+      } 
+      if(input.sort === 'trending') {
+        sort = '-createdAt'
+      } 
+
+      if(input.sort === 'hot_and_new') {
+        sort = "+createdAt"
+      }
+
+
 
       if (input.minPrice && input.maxPrice) {
         where.price = {
@@ -53,7 +71,6 @@ export const productsRouter = createTRPCRouter({
           }
         })
 
-        console.log(JSON.stringify(categoriesData, null, 2))
 
         const formattedData = categoriesData.docs.map((doc) => {
           const category = doc as CategoryWithSubcategories;
@@ -70,8 +87,6 @@ export const productsRouter = createTRPCRouter({
         const subcategoriesSlugs = []
         const parentCategory = formattedData[0]
 
-
-
         if (parentCategory) {
           subcategoriesSlugs.push(
             ...parentCategory.subcategories.map((subcategory) => subcategory.slug)
@@ -81,14 +96,21 @@ export const productsRouter = createTRPCRouter({
             in: [parentCategory.slug, ...subcategoriesSlugs]
           }
         }
-
-
       }
+
+      if (input.tags && input.tags.length > 0) {
+        where["tags.name"] = {
+          in: input.tags
+        }
+      }
+
+
 
       const data = await ctx.db.find({
         collection: "products",
         depth: 1, // Populate "Category" & "Image"
         where,
+        sort,
       });
 
       return data;
